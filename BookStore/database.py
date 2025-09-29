c9d7671e98839587bbfa69cdc8a623f90349350d
42nd soft gmaer <topkaki@gmail.com>
2025-09-29 18:53:58 +0100

    coming along pretty well. PS If your SQLite3 database doesn't seem to do anything, make sure you commit in your python file, such as self.connection.commit() in database.py - Have a good day folks!

--- BookStore/database.py
+++ BookStore/database.py
@@ -16,6 +16,17 @@ class DataBase():
             SURNAME text,
             BORROWED text
         );""")    
+    def add_members(self, name, surname):
+        self.cursor.execute("""INSERT INTO members
+                            (NAME, SURNAME, BORROWED)
+                            values (?, ?, ?)        
+                            """, (name, surname, None))
+        self.connection.commit() # If your database stops working for no explaiable reason, make sure you commit your changes
+    def update_members(self, borrowed, name):
+        self.cursor.execute("""UPDATE members 
+            SET BORROWED = ?
+        WHERE NAME LIKE ?;""", (borrowed, name))
+        self.connection.commit()
     def create_table_borrowed_books(self):
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS borrowed (
             id integer primary key autoincrement,
@@ -31,9 +42,24 @@ class DataBase():
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS books (
             book_id integer primary key autoincrement,
             NAME text,
+            AUTHOR text,
             ISBN text,
             EDITION text
         );""")      
+    def add_books(self, name, author, isbn, edition):
+        self.cursor.execute("""INSERT INTO books
+                            (NAME, AUTHOR, ISBN, EDITION)
+                            values (?, ?, ?, ?)     
+                            """, (name, author, isbn, edition))
+        self.connection.commit() # Will have to change to make it the correct values
+    def update_books(self, name, author, isbn, edition):
+        self.cursor.execute("""UPDATE books 
+            SET NAME = ?,
+                AUTHOR = ?,
+                ISBN = ?,
+                EDITION = ?
+        WHERE NAME LIKE ?;""", (name, author, isbn, edition, name))
+        self.connection.commit()
     def fetch(self, tablename):
         self.cursor.execute(f"""SELECT * FROM {tablename}
         """)
--- BookStore/main.py
+++ BookStore/main.py
@@ -1,21 +1,41 @@
 import database
 data = database.DataBase()
-#member = Member()
+
+
 
 class Member():
     def __init__(self, name, surname, borrowed):
         self.name = name
         self.surname = surname
         self.borrowed = borrowed
+        #self.member_id = member_id
+        data.create_table_members()
     def list_members(self):
         lists = data.fetch("members")
         return lists
-    
+    def add_member(self):
+        data.add_members(self.name, self.surname)
+    def update_member(self):
+        data.update_members(self.borrowed, self.name)
 class Books():
-    def __init__(self, title, isbn, edition):
+    def __init__(self, title, author, isbn, edition):
         self.title = title
+        self.author = author
         self.isbn = isbn
         self.edition = edition
+        data.create_table_books()
     def list_books(self):
         lists = data.fetch("books")
         return lists
+    def add_book(self):
+        data.add_books(self.title, self.author, self.isbn, self.edition)
+    def update_book(self):
+        data.update_books(self.title, self.author, self.isbn, self.edition)        
+book = Books("Les Misérables", "Victor Hugo", "978-2-211-23864-5", "Édition de Référence")
+book.add_book()
+book.title = 'Les Mis'
+input("Press ENTER to continue...")
+book.update_book()
+memb = Member("Jean", "France", "Paul-Sartre Jean: L'être et le néant.")
+memb.add_member()
+memb.update_member()
