6875637c7758a294dbf653159b56537eb9246ac6
42nd soft gmaer <topkaki@gmail.com>
2025-10-13 19:05:15 +0100

    ain't forgetting to commit today

--- BookStore/database.py
+++ BookStore/database.py
@@ -13,22 +13,21 @@ class DataBase():
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS members (
             member_id integer primary key autoincrement,
             NAME text,
-            SURNAME text,
-            BORROWED text
+            SURNAME text
         );""")    
     def add_members(self, name, surname):
         self.cursor.execute("""INSERT INTO members
-                            (NAME, SURNAME, BORROWED)
-                            values (?, ?, ?)        
-                            """, (name, surname, None))
+                            (NAME, SURNAME)
+                            values (?, ?)        
+                            """, (name, surname, ))
         self.connection.commit() # If your database stops working for no explaiable reason, make sure you commit your changes
-    def update_members(self, borrowed, name):
-        self.cursor.execute("""UPDATE members 
-            SET BORROWED = ?
-        WHERE NAME LIKE ?;""", (borrowed, name))
-        self.connection.commit()
+    #def update_members(self, borrowed, name):
+    #    self.cursor.execute("""UPDATE members 
+    #        SET BORROWED = ?
+    #    WHERE NAME LIKE ?;""", (borrowed, name))
+    #    self.connection.commit()
     def fetch_member(self, name):
-        self.cursor.execute(f"""SELECT ID FROM books
+        self.cursor.execute(f"""SELECT member_id FROM members
                             WHERE NAME LIKE ?;
         """, (name,))
         rows = self.cursor.fetchall()
@@ -44,6 +43,22 @@ class DataBase():
             FOREIGN KEY(member_id) REFERENCES members(member_id),
             FOREIGN KEY(book_id) REFERENCES books(book_id)
         );""")  # BORROWED = date when it was borrowed
+    def add_borrow_list(self, member_id, book_id, borrowed_date, returndate, return_status,):
+        self.cursor.execute("""SELECT RETURN_STATUS FROM borrowed
+                            WHERE book_id like ?
+                            """, (book_id,))
+        rows = self.cursor.fetchall()
+        print("DEBUG: VARIABLES IN DATABASE.PY ADD BORROW LIST FUNC TION")
+        print(book_id)
+        print(rows)
+        if (0,) in rows:
+            print("You ain't supposed to borrow that kid!")
+            return "You ain't supposed to borrow that kid!"
+        self.cursor.execute("""INSERT INTO borrowed
+                            (member_id, book_id, BORROWED_DATE, RETURNDATE, RETURN_STATUS)
+                            values (?, ?, ?, ?, 0)     
+                            """, (member_id, book_id, borrowed_date, returndate, ))
+        self.connection.commit() # Will have to change to make it the correct values
     def create_table_books(self):
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS books (
             book_id integer primary key autoincrement,
@@ -54,11 +69,32 @@ class DataBase():
             IS_BORROWED integer default 0
         );""")      
     def add_books(self, name, author, isbn, edition):
-        self.cursor.execute("""INSERT INTO books
-                            (NAME, AUTHOR, ISBN, EDITION, IS_BORROWED)
-                            values (?, ?, ?, ?, 0)     
-                            """, (name, author, isbn, edition))
-        self.connection.commit() # Will have to change to make it the correct values
+        self.cursor.execute("""SELECT 1 FROM books
+                    WHERE NAME = ?
+                            """, (name,))
+        rows = self.cursor.fetchall()
+        if not rows:
+            self.cursor.execute("""INSERT INTO books
+                                (NAME, AUTHOR, ISBN, EDITION, IS_BORROWED)
+                                values (?, ?, ?, ?, 0)     
+                                """, (name, author, isbn, edition))
+            self.connection.commit() # Will have to change to make it the correct values
+    def member_borrowed_books(self, name):
+
+        self.cursor.execute("""SELECT member_id FROM members
+                                WHERE NAME LIKE ? 
+                            """, (name,))
+        rows = self.cursor.fetchall()
+        if not rows:
+            return
+        else:
+            member_id = rows[0][0]
+        self.cursor.execute("""SELECT book_id FROM borrowed
+                                WHERE member_id LIKE ? and RETURN_STATUS is 0
+                            """, (member_id,))
+        rows = self.cursor.fetchall()
+        return rows
+
     def update_books(self, name, author, isbn, edition, status):
         self.cursor.execute("""UPDATE books 
             SET NAME = ?,
@@ -69,7 +105,7 @@ class DataBase():
         WHERE NAME LIKE ?;""", (name, author, isbn, edition, status, name))
         self.connection.commit()
     def fetch_book_id(self, name):
-        self.cursor.execute(f"""SELECT ID FROM books
+        self.cursor.execute(f"""SELECT book_id FROM books
                             WHERE NAME LIKE ?;
         """, (name,))
         rows = self.cursor.fetchall()
--- BookStore/main.py
+++ BookStore/main.py
@@ -1,9 +1,8 @@
 import database
-import time
+import datetime
 data = database.DataBase()
-
-
-
+today = datetime.datetime.now()
+today = today.strftime("%d/%m/%Y")
 class Member():
     def __init__(self, name, surname, borrowed):
         self.name = name
@@ -16,10 +15,17 @@ class Member():
         return lists
     def add_member(self):
         data.add_members(self.name, self.surname)
-    def update_member(self):
-        data.update_members(self.borrowed, self.name)
+    #def update_member(self):
+    #    data.update_members(self.borrowed, self.name)
     def get_member_id(self):
-        return data.fetch_member(self.name)[0][0]
+        fetchmember = data.fetch_member(self.name)
+        if fetchmember:
+            return fetchmember[0][0]
+        else:
+            return "Get Member ID hasn't returned anything :("
+    def get_borrowed_books(self):
+        fetchbooks = data.member_borrowed_books(self.name)
+        return fetchbooks
 class Books():
     def __init__(self, title, author, isbn, edition):
         self.title = title
@@ -32,7 +38,9 @@ class Books():
         lists = data.fetch("books")
         return lists
     def get_book_id(self):
-        return data.fetch_book_id(self.title)[0][0]
+        getbookid = data.fetch_book_id(self.title)
+        if getbookid:
+            return getbookid[0][0]
     def add_book(self):
         data.add_books(self.title, self.author, self.isbn, self.edition)
     def get_status(self):
@@ -55,5 +63,9 @@ book.update_status()
 book.update_book()
 memb = Member("Jean", "France", "Paul-Sartre Jean: L'être et le néant.")
 memb.add_member()
-memb.update_member()
-data.add_borrow_list(memb.get_member_id, book.get_book_id, time.asctime()[0:10] + " " + time.asctime()[-4:], "06/07/2027", False)
+#memb.update_member()
+print(memb.get_member_id())
+print(book.get_book_id())
+a = data.add_borrow_list(str(memb.get_member_id()), str(book.get_book_id()), today, "06/07/2027", False,)
+print(a)
+print(memb.get_borrowed_books())
